807ac7bd22a937550b597d61c831d1b8ae658903
Tyler Chi <itychi@gmail.com>
2018-04-09 21:12:45 -0700

    yay

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import Navbar from './components/navbar';
+import Mainarea from './components/mainarea';
+
 
 
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div style={{height: '100%'}}>
         <Navbar />
+        <Mainarea />
       </div>
     );
   }
---
+++ src/components/mainarea.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../stylesheets/mainarea.css';
+
+class Mainarea extends React.Component {
+
+    render(){
+        return (
+            <div className = 'main-area'>
+                Hello
+            </div>
+        );
+    }
+}
+
+export default Mainarea;
