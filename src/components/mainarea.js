9252ed0a7b66d8f1b1e12efefbe547ea37ae5e3c
Tyler Chi <itychi@gmail.com>
2018-04-09 22:04:38 -0700

    yay

--- src/components/mainarea.js
+++ src/components/mainarea.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import '../stylesheets/mainarea.css';
+import Solver from './solver';
+
 
 class Mainarea extends React.Component {
 
     render(){
         return (
             <div className = 'main-area'>
+                <Solver />
             </div>
         );
     }
--- src/components/solver.js
+++ src/components/solver.js
@@ -1,2 +1,24 @@
 import React from 'react';
+import '../stylesheets/solver.css';
 
+class Solver extends React.Component {
+
+
+
+    render(){
+
+        return(
+            <div className="solver">
+
+                <div className = "solver-area">
+                
+                    I am the solver
+
+                </div>
+        
+            </div>
+        );
+    }
+}
+
+export default Solver;
