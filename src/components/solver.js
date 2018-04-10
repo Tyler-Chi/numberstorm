12ffac247427ff5a76628b856d6972d5c1c6f306
Tyler Chi <itychi@gmail.com>
2018-04-10 00:10:33 -0700

    please

--- src/components/solver.js
+++ src/components/solver.js
@@ -1,25 +1,36 @@
 import React from 'react';
 import '../stylesheets/solver.css';
+import axios from 'axios';
 
 class Solver extends React.Component {
 
     constructor(props){
         super(props);
         this.state = {
-            num1: 0,
-            num2: 0,
-            num3: 0,
-            num4: 0
+            numbers: [0,0,0,0],
+            solution: ''
         };
 
         this.handleChange = this.handleChange.bind(this);
-
+        this.handleSubmit = this.handleSubmit.bind(this);
+    
     }
 
-    handleChange(event){
-        this.setState({num1: event.target.value});
+    
+
+    handleChange(event, index){
+        let currentNumbers = this.state.numbers;
+        currentNumbers[index] = event.target.value;
+        this.setState({numbers: currentNumbers});
     }
 
+    handleSubmit(){
+        let currentNumberString = this.state.numbers.join('&');
+        axios.get('https://mathology.herokuapp.com/api/solutions')
+            .then(res => {
+                console.log(res);
+            })
+    }
 
 
     render(){
@@ -30,7 +41,12 @@ class Solver extends React.Component {
             <div className="solver">
                     <p className="solver-title"> See if 4 numbers can make 24! </p>
 
-                    <input type='integer' value={this.state.num1} onChange={this.handleChange}/> 
+                    <input type='integer' value={this.state.numbers[0]} onChange={(event) => this.handleChange(event, 0)}/> 
+                    <input type='integer' value={this.state.numbers[1]} onChange={(event) => this.handleChange(event, 1)} /> 
+                    <input type='integer' value={this.state.numbers[2]} onChange={(event) => this.handleChange(event, 2)} /> 
+                    <input type='integer' value={this.state.numbers[3]} onChange={(event) => this.handleChange(event, 3)} /> 
+
+                    <p onClick = {this.handleSubmit}> SUBMIT </p> 
 
               
             </div>
