e5a5b56ee2b6cd5ee037265327539e293e7724b1
Tyler Chi <itychi@gmail.com>
2018-04-09 22:40:16 -0700

    got part of the form working

--- src/components/solver.js
+++ src/components/solver.js
@@ -3,19 +3,36 @@ import '../stylesheets/solver.css';
 
 class Solver extends React.Component {
 
+    constructor(props){
+        super(props);
+        this.state = {
+            num1: 0,
+            num2: 0,
+            num3: 0,
+            num4: 0
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+
+    }
+
+    handleChange(event){
+        this.setState({num1: event.target.value});
+    }
+
 
 
     render(){
 
+        console.log(this.state);
+
         return(
             <div className="solver">
+                    <p className="solver-title"> See if 4 numbers can make 24! </p>
 
-                <div className = "solver-area">
-                
-                    I am the solver
+                    <input type='integer' value={this.state.num1} onChange={this.handleChange}/> 
 
-                </div>
-        
+              
             </div>
         );
     }
